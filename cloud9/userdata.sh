e2f011f9027693b33902b6428000498faf38e190
Tim Bryant <timothybryant3@gmail.com>
2020-09-25 18:35:07 +0000

    update versions of ruby and other dependencies

--- cloud9/userdata.sh
+++ cloud9/userdata.sh
@@ -12,9 +12,14 @@ chmod 755 /home/ec2-user
 
 sudo -u ec2-user curl -L https://raw.githubusercontent.com/c9/install/master/install.sh | bash
 
-systemctl enable postgresql.service
-postgresql-setup initdb
-systemctl start postgresql.service
+#systemctl enable postgresql.service
+#postgresql-setup initdb
+#systemctl start postgresql.service
+
+sudo service postgresql-setup initdb
+sudo service postgresql start
+
+#curl -L https://raw.githubusercontent.com/timmyb824/newrelic-ruby-kata/master/cloud9/userdata.sh | bash
 
 sudo -u postgres createuser -s ec2-user
 sudo -u postgres createdb ec2-user
@@ -22,8 +27,9 @@ sudo -u postgres psql -c "ALTER USER \"ec2-user\" WITH SUPERUSER;"
 
 sed -i -e "s/host    all             all             127.0.0.1\/32            ident/host    all             all             127.0.0.1\/32            trust/g" /var/lib/pgsql/data/pg_hba.conf
 
-systemctl restart postgresql.service
+#systemctl restart postgresql.service
+sudo service postgresql restart
 
-git clone https://github.com/newrelic/newrelic-ruby-kata /home/ec2-user/newrelic-ruby-kata
+#git clone https://github.com/timmyb824/newrelic-ruby-kata /home/ec2-user/newrelic-ruby-kata
 
 chown ec2-user.ec2-user -R /home/ec2-user/newrelic-ruby-kata
